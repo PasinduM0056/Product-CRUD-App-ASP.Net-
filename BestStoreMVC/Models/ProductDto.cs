a1717517ca02ba22c5f71dc1c8b3b2658015e0c5
Malinda <malindafiv56@gmail.com>
2024-03-09 23:47:20 +0530

    All Commit

---
+++ BestStoreMVC/Models/ProductDto.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public string Price { get; set; } = "";
+
+        [Required]
+        public string Description { get; set; } = "";
+
+        public IFormFile? ImageFile { get; set; } 
+    }
+}
